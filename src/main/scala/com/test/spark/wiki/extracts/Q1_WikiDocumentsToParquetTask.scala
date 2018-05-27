55688eb1b69072d519561f2875595233d9b0f22d
rania <rania@armadillo.fr>
2018-05-27 23:57:50 +0200

    first version

--- src/main/scala/com/test/spark/wiki/extracts/Q1_WikiDocumentsToParquetTask.scala
+++ src/main/scala/com/test/spark/wiki/extracts/Q1_WikiDocumentsToParquetTask.scala
@@ -8,8 +8,10 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.joda.time.DateTime
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.slf4j.{Logger, LoggerFactory}
-
+import java.io.{File, FileInputStream}
+import scala.collection
 import scala.collection.JavaConversions._
 
 case class Q1_WikiDocumentsToParquetTask(bucket: String) extends Runnable {
@@ -22,7 +24,7 @@ case class Q1_WikiDocumentsToParquetTask(bucket: String) extends Runnable {
 
     getLeagues
       // TODO Q1 Transformer cette seq en dataset
-      .XXXXX
+      .toDS()
       .flatMap {
         input =>
           (fromDate.getYear until toDate.getYear).map {
@@ -38,15 +40,51 @@ case class Q1_WikiDocumentsToParquetTask(bucket: String) extends Runnable {
             //  - Quelques pages ne respectent pas le format commun et pourraient planter - pas grave
             //  - Il faut veillez à recuperer uniquement le classement général
             //  - Il faut normaliser les colonnes "positions", "teams" et "points" en cas de problèmes de formatage
+
+
+            val doc =  Jsoup.connect(url).get()
+             val table =    doc.select("table:has(caption:contains(Classement))")
+            val ite = table.select("td").iterator
+            val element=table.select("td")
+            if(element.size()==10){
+              LeagueStanding(league,season,position=ite.next().text().toInt,team= ite.next().text(),points=ite.next().text().toInt,played=ite.next().text().toInt,
+                won=ite.next().text().toInt,drawn=ite.next().text().toInt,lost=ite.next().text().toInt,goalsFor=ite.next().text().toInt,goalsAgainst=ite.next().text().toInt,goalsDifference=ite.next().text().toInt)
+            }
+
+
+
+
+
           } catch {
             case _: Throwable =>
               // TODO Q3 En supposant que ce job tourne sur un cluster EMR, où seront affichés les logs d'erreurs ?
+              /****
+                * Spark Jobs logs:
+                *
+                * S3: s3://aws-logs-[accound-id]-region /elasticmapreduce//[master-id]/containers/
+                *
+                *
+                *
+                * Spark Server:
+                *
+                * EMR Master node: /var/log/spark/spark-history-server.out
+                *
+                * S3: s3://aws-logs-[accound-id]-region/elasticmapreduce/[master-id]/node/[ec2-id]/applications/spark/spark-history-server.out.gz
+                *
+                *
+                *
+                * YARN :
+                *
+                * EMR Master node: /mnt/var/log/hadoop-yarn
+                *
+                * S3: s3://aws-logs-736182580110-eu-west-1/elasticmapreduce/[master-id]/node/[ec2-id /applications/hadoop-yarn/
+                */
               logger.warn(s"Can't parse season $season from $url")
               Seq.empty
           }
       }
       // TODO Q4 Comment partitionner les données en 2 avant l'écriture sur le bucket
-      .XXXXX
+      .repartition(2)
       .write
       .mode(SaveMode.Overwrite)
       .parquet(bucket)
@@ -54,18 +92,31 @@ case class Q1_WikiDocumentsToParquetTask(bucket: String) extends Runnable {
     // TODO Q5 Quel est l'avantage du format parquet par rapport aux autres formats ?
 
     // TODO Q6 Quel est l'avantage de passer d'une séquence scala à un dataset spark ?
+    /*
+    Une sequence en Scala est une structure de données classique qui permet de stocker des objets dans une liste ordonnée
+et qui offre des opérations d'ajout, suppression,..
+D'autre part une API Dataset permet de:
+-interroger  les données en utilisant SQL
+-traiter les données d'une façons distribués et paralléle
+
+     */
   }
+
+
+  d
 
   private def getLeagues: Seq[LeagueInput] = {
     val mapper = new ObjectMapper(new YAMLFactory())
     // TODO Q7 Recuperer l'input stream du fichier leagues.yaml
-    mapper.readValue(inputStream, classOf[Array[LeagueInput]]).toSeq
+    val yamlfile = getClass.getResource("/leagues.yaml").getPath
+    val input = new FileInputStream(new File(yamlfile))
+      mapper.readValue(input, classOf[Array[LeagueInput]]).toSeq
   }
 }
 
 // TODO Q8 Ajouter les annotations manquantes pour pouvoir mapper le fichier yaml à cette classe
-case class LeagueInput(name: String,
-                       url: String)
+case class LeagueInput(@JsonProperty("name") name: String,
+@JsonProperty ("url") url: String)
 
 case class LeagueStanding(league: String,
                           season: Int,
--- src/main/scala/com/test/spark/wiki/extracts/Q2_ShowLeagueStatsTask.scala
+++ src/main/scala/com/test/spark/wiki/extracts/Q2_ShowLeagueStatsTask.scala
@@ -21,13 +21,24 @@ case class Q2_ShowLeagueStatsTask(bucket: String) extends Runnable {
     println("Utiliser createTempView sur $standings et, en sql, afficher la moyenne de buts par saison et " +
       "par championnat")
 
+    standings.createOrReplaceTempView("LeagueTable")
+
+    session.sql("SELECT season, league,avg(goalsFor) as  goalsAvg from LeagueTable group by league,season ").show()
+
+
     // TODO Q2
     println("En Dataset, quelle est l'équipe la plus titrée de France ?")
+    val teamTitre = standings.filter(x=> (x.position==1 && x.league.toLowerCase=="ligue 1"))
+      .groupByKey(_.team).count()
+      .orderBy(desc("count(1)")).first()._1
+
 
     // TODO Q3
     println("En Dataset, quelle est la moyenne de points des vainqueurs sur les 5 différents championnats ?")
 
     // TODO Q5 Ecrire une udf spark "decade" qui retourne la décennie d'une saison sous la forme 199X ?
+    
+    val decade= udf((x: Int) =>)
 
     // TODO Q4
     println("En Dataset, quelle est la moyenne de points d'écart entre le 1er et le 10ème de chaque championnats " +
